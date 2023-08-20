cd0b585ae14ecc88b180d6192b5d7475e3f11c14
Vidyut <vidyutlearning07@gmail.com>
2023-08-20 15:20:28 +0530

    Added worlist

--- src/constants/rewardsList.ts
+++ src/constants/rewardsList.ts
@@ -1,18 +1,38 @@
 export const REWARD_LIST = [
-    {
-        key: 'ACH',
-        value: 'Automated Clearing House',
-        def: 'A system used in the USA that acts as a hub to facilitate electronic transfers between bank accounts and is a type of Electronic Funds Transfer (ETF). It allows for seamless transfer of funds between accounts.'
-    },
-    {
-        key: 'AI',
-        value: 'Artificial Intelligence',
-        def: 'Computer systems capable of performing tasks that traditionally require human intelligence, such as reasoning, decision-making, and perception. AI enhances fraud detection and risk management within digital payments.'
-    },    
-    {
-        key: '',
-        value: 'Acquirer',
-        def: 'Financial entities that handle and settle transactions on behalf of other merchants involved. It ensures that transactions are processed accurately and efficiently.'
-    }
-    //TODO: Add further more list from Wordle Definitions.txt file
+  {
+    key: 'ACH',
+    value: 'Automated Clearing House',
+    def: 'A system used in the USA that acts as a hub to facilitate electronic transfers between bank accounts and is a type of Electronic Funds Transfer (ETF). It allows for seamless transfer of funds between accounts.',
+  },
+  {
+    key: 'AI',
+    value: 'Artificial Intelligence',
+    def: 'Computer systems capable of performing tasks that traditionally require human intelligence, such as reasoning, decision-making, and perception. AI enhances fraud detection and risk management within digital payments.',
+  },
+  {
+    key: '',
+    value: 'Acquirer',
+    def: 'Financial entities that handle and settle transactions on behalf of other merchants involved. It ensures that transactions are processed accurately and efficiently.',
+  },
+  {
+    key: '',
+    value: 'Account Information Service Provider',
+    def: "An AISP provides third-party access to financial account information with the account holder's permission to provide faster decisions on loans. This enables faster digital payments.",
+  },
+  {
+    key: '',
+    value: 'Alternative Data',
+    def: 'Non-traditional/ novel sources of data (e.g., social media activity or satellite imagery) that can provide insights in order to improve financial decision-making. Diverse data helps to detect fraud, understand consumer behaviour and provide personalised recommendations.',
+  },
+  {
+    key: '',
+    value: 'Anti-Money Laundering',
+    def: 'Describes a set of procedures and regulations that can detect and report illegal income sources in money laundering. AML prevents money laundering from damaging digital payment spaces.',
+  },
+  {
+    key: '',
+    value: 'Application Programming Interface',
+    def: 'A set of protocols, routines, and tools that enable software applications to communicate with one another, allowing for data sharing and service integration. APIs enable payment platforms to communicate together.',
+  },
+  //TODO: Add further more list from Wordle Definitions.txt file
 ]
